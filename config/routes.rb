96b6dcdabccebfec19ce55f059f847b31c4b0ce8
Ryan Yu <ryan.cao.you@gmail.com>
2015-02-28 23:17:22 +0000

    RY: Reverted back to working authentication and fixed flash

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   
   resources :articles
   
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  
   resources :users do
     resources :articles
   end
-  
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   
   root 'articles#index'
 
