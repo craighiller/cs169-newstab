1d26e071d57a228a9034455c676c4ccb3e9497dc
Ryan Yu <ryan.cao.you@gmail.com>
2015-03-14 05:44:16 +0000

    Add rspec

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -6,7 +6,7 @@ describe ArticlesController do
   # This should return the minimal set of attributes required to create a valid
   # Article. As you add validations to Article, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { { "concept" => "MyString" } }
+  let(:valid_attributes) { { "url" => "MyURL", "initial_comment" => "hello" , "user_id" => 1} }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -2,6 +2,15 @@ require 'spec_helper'
 
 describe CommentsController do
 
+  it 'should post the comment in the database' do
+    Comments.create(:find)
+    get :index
+  end
+  
+  it "should display the name of the person who posted the comment" do
+    
+
+
   # describe "GET 'create'" do
   #   it "returns http success" do
   #     get 'create'
